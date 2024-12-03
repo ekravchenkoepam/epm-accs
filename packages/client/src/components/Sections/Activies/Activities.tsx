48996abffdd4e68e4a86e09918ed1ae667fc6dd2
ekravchenkoepam <yevhen_kravchenko@epam.com>
2024-12-03 11:09:22 +0200

    [US-003] - Add Industry Section

--- packages/client/src/components/Sections/Activies/Activities.tsx
+++ packages/client/src/components/Sections/Activies/Activities.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 
 import { Section } from "../../Section";
+import { InputButtonGroup } from "../../InputButtonGroup";
+import { RadioButton } from "../../../elements/RadioButton";
+import { CheckboxButton } from "../../../elements/Checkbox";
+
 import { CommonSectionProps } from "../types";
+import { ADDITIONAL_ACTIVITIES, ASSESSMENT_TYPES } from "../constants";
+import { SECTIONS_LIST } from "../SectionsList/constants";
+
+import styles from '../../Section/Section.module.css'
+import locales from "../../../locales/en.json";
 
 export const Activities: React.FC<CommonSectionProps> = ({ sectionType }) => {
   return (
     <Section sectionType={sectionType}>
-      <div></div>
+      <div className={styles.optionBlocks}>
+        <InputButtonGroup
+          label={locales.activities.assessmentTypeLabel}
+          options={Object.values(ASSESSMENT_TYPES)}
+          sectionType={SECTIONS_LIST.ACTIVITIES}
+          infoText={locales.activities.assessmentTypeHint}
+          ButtonComponent={RadioButton}
+        />
+        <InputButtonGroup
+          label={locales.activities.additionalActivities}
+          options={Object.values(ADDITIONAL_ACTIVITIES)}
+          sectionType={SECTIONS_LIST.ACTIVITIES}
+          infoText={locales.activities.additionalActivitiesHint}
+          ButtonComponent={CheckboxButton}
+        />
+      </div>
     </Section>
   )
 }
